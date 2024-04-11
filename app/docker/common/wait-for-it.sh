77a70b1442380c824666c6c661af3a14d936ce80
Olexander <saschatimail@gmail.com>
2024-04-11 16:38:42 +0300

    feet: Test change some by commit step

--- app/docker/common/wait-for-it.sh
+++ app/docker/common/wait-for-it.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Use this script to test if a given TCP host/port are available
 
+# Change some in file 1
+
 WAITFORIT_cmdname=${0##*/}
 
 echoerr() { if [[ $WAITFORIT_QUIET -ne 1 ]]; then echo "$@" 1>&2; fi }
